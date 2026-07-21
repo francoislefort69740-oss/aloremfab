ee4f35ca260bb342e7fe67c5ea50f18dc71eee3f
qualite <francois.lefort69740@gmail.com>
2026-07-21 11:08:57 +0200

    Define rules for showing or not bottom floating button into control panel

--- app/src/main/java/com/example/myapplication/childfragment/ChildControlGRVViewPagerFragment.kt
+++ app/src/main/java/com/example/myapplication/childfragment/ChildControlGRVViewPagerFragment.kt
@@ -4,55 +4,44 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.constraintlayout.widget.Guideline
 import com.example.myapplication.R
 import com.example.myapplication.callback.ChildViewPagerGRVInterface
+import com.example.myapplication.component.GRVControlAddingPage
+import com.example.myapplication.component.GRVControlProcess
 import com.example.myapplication.fragment.BaseFragment
 import com.example.myapplication.model.ControlGRV
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ChildControlGRVViewPagerFragment: BaseFragment() {
     override fun getLayout(): Int = R.layout.fragment_child_control_grv_view_pager
 
+    private val controlComponent: GRVControlProcess = GRVControlProcess()
+    private val addingPageComponent: GRVControlAddingPage = GRVControlAddingPage()
+
     override fun getBody(view: View, savedInstanceState: Bundle?) {
         arguments?.let { arguments ->
-            val isAddingPage = arguments.getInt(GRV_PAGE_ID) == 0
-            setUpView(isAddingPage = isAddingPage, view = view)
-            if (isAddingPage){ // C'est la page d'ajout de contrôle
+            if (arguments.getInt(GRV_PAGE_ID) == 0){ // C'est la page d'ajout de contrôle
 
-                view.findViewById<ImageView>(R.id.add_child_control_grv).setOnClickListener {
+                addingPageComponent.setUp(view = view, arguments = arguments)
+
+                addingPageComponent.addingButton().setOnClickListener {
                     mCallback?.createNewPage()
                 }
 
             } else { // c'est une page de contrôle existante
 
-                view.findViewById<TextView>(R.id.step_title_child_control_grv).text = arguments.getInt(GRV_PAGE_ID).toString()
-                view.findViewById<ImageView>(R.id.close_child_control_grv).setOnClickListener {
-                    mCallback?.deleteControl(arguments.getInt(GRV_PAGE_ID))
+                controlComponent.setUp(view = view, arguments = arguments)
+
+                controlComponent.closeButton().setOnClickListener {
+                    mCallback?.deleteControl(pos = controlComponent.getPageId())
                 }
             }
         }
     }
 
-    private fun setUpView(isAddingPage: Boolean, view: View) {
-        val mVisibility = if (isAddingPage) View.GONE else View.VISIBLE
-        view.findViewById<TextView>(R.id.step_title_child_control_grv).visibility = mVisibility
-        view.findViewById<FloatingActionButton>(R.id.check_all_ok_child_control_grv).visibility = mVisibility
-        view.findViewById<FloatingActionButton>(R.id.save_child_control_grv).visibility = mVisibility
-        view.findViewById<FloatingActionButton>(R.id.back_child_control_grv).visibility = mVisibility
-        view.findViewById<FloatingActionButton>(R.id.next_child_control_grv).visibility = mVisibility
-        view.findViewById<ImageView>(R.id.close_child_control_grv).visibility = mVisibility
-        val mNotVisibility = if (isAddingPage) View.VISIBLE else View.GONE
-        view.findViewById<ImageView>(R.id.add_child_control_grv).visibility = mNotVisibility
-        val posGuideLine = if (isAddingPage) 0.68 else 0.9
-        view.findViewById<Guideline>(R.id.menu_guideline_3_child_control_grv).setGuidelinePercent(posGuideLine.toFloat())
-    }
-
     companion object {
         fun newInstance(controlGRV: ControlGRV) = ChildControlGRVViewPagerFragment().apply {
             arguments = Bundle().apply {
-                controlGRV.uid?.let { putInt(GRV_ID, it)}
+                putInt(GRV_ID, controlGRV.uid)
                 putInt(GRV_PAGE_ID, controlGRV.pageId)
                 controlGRV.currentStep?.id?.let { putInt(GRV_CURRENT_STEP, it) }
             }
--- app/src/main/java/com/example/myapplication/model/ControlGRV.kt
+++ app/src/main/java/com/example/myapplication/model/ControlGRV.kt
@@ -2,7 +2,7 @@ package com.example.myapplication.model
 
 data class ControlGRV(
     val pageId: Int,
-    val uid: Int? = 0,
-    val serialNumber: Int? = 0,
+    val uid: Int = 0,
+    val serialNumber: Int = 0,
     var currentStep: StepControlGRV? = null
 )
